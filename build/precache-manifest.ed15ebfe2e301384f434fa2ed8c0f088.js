736c1121e186cd9a3833cc79d0fa4caf527c0039
nbauer <nbauer@vrbo.com>
2021-01-18 17:09:49 -0600

    Adding all the contents of react blog

---
+++ build/precache-manifest.ed15ebfe2e301384f434fa2ed8c0f088.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0267332010d72ff6ab7c1502252453ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "142f63574e8559a1f4cf",
+    "url": "/static/css/3.17727e1f.chunk.css"
+  },
+  {
+    "revision": "f05c6e7e5a6f90949532",
+    "url": "/static/css/4.f0478b6e.chunk.css"
+  },
+  {
+    "revision": "f111c990715d57841cd9",
+    "url": "/static/css/5.15f7c63d.chunk.css"
+  },
+  {
+    "revision": "42011e933529affe35d7",
+    "url": "/static/css/main.5cadecfa.chunk.css"
+  },
+  {
+    "revision": "0d2f47de0dd2386cc6d4",
+    "url": "/static/js/2.6a420053.chunk.js"
+  },
+  {
+    "revision": "142f63574e8559a1f4cf",
+    "url": "/static/js/3.a549e219.chunk.js"
+  },
+  {
+    "revision": "f05c6e7e5a6f90949532",
+    "url": "/static/js/4.f7a4caa8.chunk.js"
+  },
+  {
+    "revision": "f111c990715d57841cd9",
+    "url": "/static/js/5.ad4bbadd.chunk.js"
+  },
+  {
+    "revision": "cf1881ddabb7729f9d2f",
+    "url": "/static/js/6.86315266.chunk.js"
+  },
+  {
+    "revision": "c5d58d6587ae1f5e2538",
+    "url": "/static/js/7.72158fa6.chunk.js"
+  },
+  {
+    "revision": "42011e933529affe35d7",
+    "url": "/static/js/main.9d76e487.chunk.js"
+  },
+  {
+    "revision": "6911386acedbcb09d791",
+    "url": "/static/js/runtime~main.b81deacb.js"
+  },
+  {
+    "revision": "0f2f9d7f113542e08f1710d64a3aa45d",
+    "url": "/static/media/navi-logo.0f2f9d7f.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/react-logo.5d5d9eef.svg"
+  }
+]);
---
+++ node_modules/navi-scripts/README.md
@@ -0,0 +1,7 @@
+# navi-scripts
+
+Build static websites with navi, with one HTML file for each URL.
+
+## Documentation
+
+See the [Navi Website](https://frontarm.com/navi/).
